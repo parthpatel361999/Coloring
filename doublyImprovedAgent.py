5b461ab144be4d469a72f61d2a4158c91dcb1007
Rohan Shah <rohanshah01@gmail.com>
2020-12-09 22:49:34 -0500

    grayscale
    but still pain

--- doublyImprovedAgent.py
+++ doublyImprovedAgent.py
@@ -7,9 +7,13 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
 import pathlib
-#build model first
-# TODO Have not tested yet
-def modelbuilder(numlayers,):
+
+
+def augment(image):
+    image = tf.cast(image,tf.float32)
+    image = tf.image.rgb_to_grayscale(image)
+    return image
+def modelbuilder(numlayers):
     model = Sequential()
     model.add(layers.experimental.preprocessing.Rescaling(1./255, input_shape = (180,180,1)))
     model.add(layers.Conv2D(64,3,activation='tanh',use_bias = True, kernel_regularizer = tf.keras.regularizers.L1L2(l1 = 0.01, l2= 0.01)))
@@ -32,33 +36,46 @@ batch_size = 32
 img_height = img_width = 180
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    pathlib.Path(path), label_mode = None, color_mode='grayscale',
+    pathlib.Path(path), label_mode = None, validation_split=0.2,
     seed=123, image_size = (img_height,img_width),
-    batch_size = batch_size,
-    validation_split = 0.2,
     subset = "training"
 )
+
+train_ds_2 = (
+    train_ds
+    .map(augment) #add autotuning
+)
+
+
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    pathlib.Path(path),color_mode='grayscale', label_mode = None,
+    pathlib.Path(path),label_mode = None, validation_split= 0.2,
     seed=123, image_size = (img_height,img_width),
     batch_size = batch_size,
-    validation_split = 0.2,
     subset = "validation"
 )
 
+
 #check if properly grayscaled
 plt.figure(figsize=(10,10))
+
 for images in train_ds.take(1):
     for i in range(9):
         ax = plt.subplot(3,3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8").reshape((180,180)), cmap = "gray")
+        plt.imshow(images[i].numpy().astype("uint8"))
         plt.title("Is she grayscale?")
         plt.axis('off')
 plt.show()
 
-for i in range(1):
-    m = modelbuilder(i)
-    m.summary()
+for images in train_ds_2.take(1):
+    for i in range(9):
+        ax = plt.subplot(3,3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8").reshape((180,180)),cmap="gray")
+        plt.title("Is she grayscale?")
+        plt.axis('off')
+plt.show()
+
+
+
 
 
 
