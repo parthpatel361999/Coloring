22e2cf1edae7b2d4ecdeee2199a6f05bb6511f09
Rohan Shah <rohanshah01@gmail.com>
2020-12-13 00:34:18 -0500

    add some stuff
    stuff and things
    
    Co-Authored-By: niharprabhala <71557872+niharprabhala@users.noreply.github.com>
    Co-Authored-By: Parth Patel <60615655+parthpatel361999@users.noreply.github.com>
    Co-Authored-By: Rumeet Goradia <20172569+rumeetgoradia@users.noreply.github.com>

--- inpainting.py
+++ inpainting.py
@@ -7,36 +7,23 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from skimage import color, io, transform
-from keras.layers import Conv2D, UpSampling2D, Dropout
+from keras.layers import Conv2D, UpSampling2D, Dropout,Dense,MaxPooling2D
 import matplotlib.pyplot as plt
 import pathlib
 from random import randint,choices
-from string import ascii_letters, digits
+from string import ascii_uppercase
 
 def modelbuilder():
     model = Sequential()
-    model.add(layers.experimental.preprocessing.RandomRotation(0.1,input_shape=(256, 256, 3)))
-    model.add(layers.experimental.preprocessing.RandomZoom(0.1))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(Dropout(0.1))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(Dropout(0.1))
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(3, (3, 3), activation='tanh', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-
-    model.compile(optimizer='rmsprop', loss = 'mse', metrics=['accuracy'])
+    
+    model.add(Conv2D(64, (16, 16),strides = (1,1), activation='relu', padding='same',input_shape=(256, 256, 3)))
+    model.add(Dense(128,activation="relu"))
+    model.add(Dense(64,activation="relu"))
+    model.add(Dense(64,activation="relu"))
+    model.add(Dense(32,activation="relu"))
+    model.add(Conv2D(1, (32, 32), strides = (1,1) ,activation="softmax", padding='same'))
+    model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+    
     model.summary()
     return model
 
@@ -47,7 +34,8 @@ def drawText(filepath, fontsize, text,target_size):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(font='arial.ttf', size=fontsize)
     size = font.getsize(text)
-    (x,y) = (randint(fontsize,255-size[0]),randint(0,255-size[1]))
+    x = randint(size[0]+16,255-16-size[0])
+    y = randint(size[1]+16,255-16-size[1])
     color = 'rgb(0,0,0)' #black
 
     draw.text((x,y), text, fill=color, font=font)
@@ -55,17 +43,18 @@ def drawText(filepath, fontsize, text,target_size):
     #image.close()
     
     #draw expected output from model 1
-    image2 = Image.new(mode = "RGB",size = target_size, color = (255,255,255))
-    draw = ImageDraw.Draw(image2)
-    draw.text((x,y), text, fill=color, font=font)
-
-
+    
+    x0 = x + 0.5*size[0] - 8
+    y1 = y + 0.5*fontsize + 8
+    x1 = x + 0.5*size[0] + 8
+    y0 = y + 0.5*fontsize - 8
+    print((int(x0),int(y0),int(x1),int(y1)))
     #draw input for model 2
     image3 = Image.open(filepath).resize(target_size)
+
     draw = ImageDraw.Draw(image3)
-    draw.rectangle(xy = [(x,y),(x+size[0],y+size[1])], fill = "#FFFFFF",outline = "white")
-    
-    return image,image2,image3
+    draw.rectangle(xy = [(x0,y0),(x1,y1)], fill = "#FFFFFF",outline = "white")
+    return image,(int(x0),int(y0),int(x1),int(y1)),image3
 
 
 #initialization variables
@@ -78,30 +67,37 @@ expOutM1 = []
 
 testTextImages = []
 
-size = 100
+size = 1
 fontsize = 15
 
-validation_message = "val"
+validation_message = "v"
 
 i = 0
 for filename in os.listdir('flower/flower_photos'):
-    message = "message" #randomize length of message and randomize message 
+    #message = ''.join(choices(ascii_uppercase, k = 1))  #randomize length of message and randomize message 
+    message = "H"
     images = drawText('flower/flower_photos/' + filename,fontsize,message,target_size)
     textImages.append(np.array(images[0],dtype = float))
-    expOutM1.append(np.array(images[1],dtype = float))
+    temp = np.zeros(shape = (256,256,3), dtype=float)
+    x0 = images[1][0]
+    y0 = images[1][1]
+    x1 = images[1][2]
+    y1 = images[1][3]    
+    temp[y0:y1,x0:x1,:] = 1.0
+    expOutM1.append(temp)
     textImagesRectangle.append(np.array(images[2],dtype = float))
     noTextImages.append(np.array(Image.open('flower/flower_photos/' + filename).resize(target_size),dtype = float))
     i += 1
     if(i == size):
         break
 
-
 i = 0
 for filename in os.listdir('testingImages'):
     images = drawText('testingImages/' + filename,fontsize,validation_message,target_size)
+    images[0].save('testingImagesWithText/' + filename)
     testTextImages.append(np.array(images[0],dtype = float))
     i += 1
-    if(i == 15):
+    if(i == 1):
         break
 
 textImages = np.array(textImages)
@@ -113,12 +109,14 @@ testTextImages = np.array(testTextImages)
 
 textImages[:,:,:,:] /= 255
 noTextImages[:,:,:,:] /= 255
-expOutM1[:,:,:,:] /= 255
 textImagesRectangle[:,:,:,:] /= 255
+
 
 plt.imshow(textImages[0])
 plt.show()
 plt.imshow(expOutM1[0])
+plt.show()
+plt.imshow(textImagesRectangle[0])
 plt.show()
 
 testTextImages[:,:,:,:] /= 255
@@ -146,12 +144,12 @@ model 2: RGB image with white box around text.
 '''
 
 
-'''
 m = modelbuilder()
-m.fit(x=textImages[:size][:][:][:],y=expOutM1[:size][:][:][:],batch_size=20,epochs=10,validation_split=0.2,use_multiprocessing=True)
-results = m.predict(testTextImages[:15][:][:][:])
 
+m.fit(x=textImages[:size][:][:][:],y=expOutM1[:size][:][:][:],batch_size=1,epochs=10,validation_split=0,use_multiprocessing=True)
+results = m.predict(textImages[:1][:][:][:])
 
+'''
 i = 0
 for images in results: 
     plt.imshow(images)
