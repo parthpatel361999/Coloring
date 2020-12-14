00e006268e47356103ce317461a93b5c705ffba5
Parth Patel <60615655+parthpatel361999@users.noreply.github.com>
2020-12-14 14:35:15 -0500

    clean code

--- inpainting.py
+++ inpainting.py
@@ -14,27 +14,31 @@ from string import ascii_uppercase
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import Sequential, load_model
 
+# Define function to build a sequential TensorFlow model
 def modelbuilder():
     model = Sequential()
+    # Add a convolution layer with with a sigmoid activation function
     model.add(layers.Conv2D(1, (2, 2), strides = (1,1), activation='sigmoid', padding='same', input_shape=(256, 256, 3)))
-    model.compile(optimizer='rmsprop', loss = 'mse', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss = 'mse', metrics=['accuracy'])
     model.summary()
     return model
 
-def drawText(filepath, fontsize, text,target_size,color):
+# Define a function to draw customized text on images
+def drawText(filepath, fontsize, text, target_size, color):
     image = Image.open(filepath)
     image = image.resize(target_size)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(font='arial.ttf', size=fontsize)
+    # Import the arial font style
+    font = ImageFont.truetype(font = 'arial.ttf', size = fontsize)
     size = font.getsize(text)
-    x = randint(size[0]+16,255-16-size[0])
-    y = randint(size[1]+16,255-16-size[1])
-    draw.text((x,y), text, fill=color, font=font)
-    x0 = x + 0.5*size[0] - 16
-    y1 = y + 0.5*fontsize + 16
-    x1 = x + 0.5*size[0] + 16 
-    y0 = y + 0.5*fontsize - 16
-    return image,(int(x0),int(y0),int(x1),int(y1))
+    x = randint(size[0] + 16, 255 - 16 - size[0])
+    y = randint(size[1] + 16, 255 - 16 - size[1])
+    draw.text((x, y), text, fill = color, font = font)
+    x0 = x + 0.5 * size[0] - 16
+    y1 = y + 0.5 * fontsize + 16
+    x1 = x + 0.5 * size[0] + 16 
+    y0 = y + 0.5 * fontsize - 16
+    return image, (int(x0), int(y0), int(x1), int(y1))
 
 def getNeighbors(r, c, buffer):
     neighbors = [(r - buffer, c - buffer), (r - buffer, c), (r - buffer, c + buffer), (r, c - buffer),
@@ -50,7 +54,7 @@ def smooth(rgb_with_text, gray_detected_text):
     detectedPixels = []
     for r in range(256):
         for c in range(256):
-            if gray_detected_text[r, c] > 0.90:
+            if gray_detected_text[r, c] > 0.92:
                 detectedPixels.append([r, c])
     length = len(detectedPixels)
     for i in range(length):
@@ -81,7 +85,7 @@ textImages = []
 noTextImages = []
 expOutM1 = []
 testTextImages = []
-size = 100
+size = 500
 fontsize = 30
 
 i = 0
@@ -122,48 +126,55 @@ textImages[:,:,:,:] /= 255
 noTextImages[:,:,:,:] /= 255
 testTextImages[:,:,:,:] /= 255
 
-# epochs = 200
-# m = modelbuilder()
-# #es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
-# mc = ModelCheckpoint('test_model_1.h5', monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
-# history = m.fit(x=textImages[:size][:][:][:], y=expOutM1[:size][:][:][:], batch_size=25, epochs=epochs, validation_split=0.2, use_multiprocessing=True, callbacks=mc)
+
+model_name = 'test_model.h5'
+epochs = 500
+m = modelbuilder()
+#es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
+mc = ModelCheckpoint(model_name, monitor = 'val_loss', mode = 'min', verbose = 1, save_best_only = True)
+history = m.fit(x = textImages[:size][:][:][:], y = expOutM1[:size][:][:][:], batch_size = 100, epochs = epochs, validation_split = 0.2, use_multiprocessing = True, callbacks = mc)
 
 #plot history
+plt.figure(figsize = (10, 10))
+plt.subplot(1, 2, 1)
+epochs_range = range(epochs)
+plt.plot(epochs_range, history.history['accuracy'], label = 'Training Accuracy')
+plt.plot(epochs_range, history.history['val_accuracy'], label = "validation Accuracy")
+plt.legend(loc = 'lower right')
+plt.title('Accuracy Results')
 
-# plt.figure(figsize = (10,10))
-# plt.subplot(1,2,1)
-# epochs_range = range(epochs)
-# plt.plot(epochs_range, history.history['accuracy'], label = 'Training Accuracy')
-# plt.plot(epochs_range, history.history['val_accuracy'], label = "validation Accuracy")
-# plt.legend(loc = 'lower right')
-# plt.title('Accuracy Results')
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, history.history['loss'], label = 'Training Loss')
+plt.plot(epochs_range, history.history['val_loss'], label = "Validation Loss")
+plt.legend(loc = 'upper right')
+plt.title('Loss Results')
 
-# plt.subplot(1,2,2)
-# plt.plot(epochs_range, history.history['loss'], label = 'Training Loss')
-# plt.plot(epochs_range, history.history['val_loss'], label = "Validation Loss")
-# plt.legend(loc = 'upper right')
-# plt.title('Loss Results')
-
-# plt.savefig("results.png")
-# plt.show()
+plt.savefig("results.png")
+plt.show()
 
 #run predictions
-saved_model = load_model('proto1_model.h5')
+saved_model = load_model('test_model_S500E500.h5')
 results = saved_model.predict(testTextImages[:15][:][:][:])
 i = 0
 for images in results: 
     gray = color.gray2rgb(images[:, :, 0])
 
+    screen = np.array(gray)
+    screen = screen / np.max(screen)
+    for r in range(256):
+        for c in range(256):
+            if screen[r,c,0] >= 0.92:
+                screen[r, c, 0] = 1
+                screen[r, c, 1] = 1
+                screen[r, c, 2] = 1
+            else:
+                screen[r, c, 0] = 0
+                screen[r, c, 1] = 0
+                screen[r, c, 2] = 0
+    
+    io.imsave('bwThreshold/' + str(i + 1) + ".png", (screen * 255).astype('uint8'), check_contrast=False)
     io.imsave('testingImagesWithText/' + str(i + 1) + ".png", (testTextImages[i] * 255).astype('uint8'))
     io.imsave('testingImagesDetectedText/' + str(i + 1) + ".png", (gray * 255).astype('uint8'), check_contrast=False)
     smoothed = smooth(testTextImages[i], gray[:, :, 0] / np.max(gray))
     io.imsave('testingImagesRemovedText/' + str(i + 1) + ".png", (smoothed * 255).astype('uint8'))
-
-    # plt.imshow(gray)
-    # plt.savefig('results/textDetected' + str(i + 1) + ".png")
-    # plt.show()
-    # smoothed = smooth(testTextImages[i], gray[:, :, 0] / np.max(gray))
-    # plt.imshow(smoothed)
-    # plt.savefig('results/textRemoved' + str(i + 1) + ".png")
-    # plt.show()
     i +=1
