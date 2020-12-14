ebe6bf54bae268c8cf7d067f516e9679669c141b
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-12-13 20:10:31 -0500

    Create test code

---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+import pathlib
+from math import sqrt
+from sys import maxsize
+from time import time
+
+import keras.optimizers
+import numpy as np
+import tensorflow as tf
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Conv2D, Dense, MaxPooling2D, UpSampling2D
+from keras.layers.convolutional import Conv2DTranspose
+from keras.layers.core import Flatten
+from keras.models import Sequential, load_model
+from PIL import Image
+
+from common import (checkQuality2, colorDistance, convertToGrayscale,
+                    getImagePixels, getSection)
+
+testingDir = "testingImages"
+testingEntries = os.listdir(testingDir)
+
+for entry in testingEntries:
+    oP = getImagePixels("testingImages", entry, resize=(256, 256))
+    oP = oP[1:oP.shape[0] - 1, 1:oP.shape[1] - 1, :]
+    nP = getImagePixels("doublyImprovedResults", entry + "-results.png")
+    print(entry + ":", str(checkQuality2(oP, nP)))
