309e927c6347e43e0544325d2f2702c62c9feb7a
Parth Patel <60615655+parthpatel361999@users.noreply.github.com>
2020-12-14 17:58:05 -0500

    create inpainting function

--- inpainting.py
+++ inpainting.py
@@ -90,105 +90,105 @@ def smooth(rgb_with_text, gray_detected_text):
             rgb_with_text[loc[0]][loc[1]] = averagedPixel
     return rgb_with_text
 
-# Initialize variables for training and testing
-target_size = (256,256)
-textImages = []
-noTextImages = []
-expOutM1 = []
-testTextImages = []
-size = 500
-fontsize = 30
+def inpainting (modelFilePath = 'test_model_S500E500.h5', loadModel = False):
+    # Initialize variables for training and testing
+    target_size = (256,256)
+    textImages = []
+    expOutM1 = []
+    testTextImages = []
+    size = 300
+    fontsize = 30
 
-# Retrieve and process the images for the training and validation set
-i = 0
-for filename in os.listdir('flower/flower_photos'): 
-    # Radnomly generate a 4 letter word to be overlaid on an image
-    message = ''.join(choices(ascii_uppercase, k = 4))
-    # Randomly select a color for the font
-    c = 'rgb(' + str(randint(0, 255)) + ',' + str(randint(0, 255)) + ',' + str(randint(0, 255)) + ')'
-    # Draw the words on the images
-    images = drawText('flower/flower_photos/' + filename, fontsize, message, target_size, c)
-    textImages.append(np.array(images[0], dtype = float))
-    # Keep track of the location of the word for each image
-    temp = np.zeros(shape = (256, 256, 3), dtype = float)
-    x0 = images[1][0]
-    y0 = images[1][1]
-    x1 = images[1][2]
-    y1 = images[1][3]    
-    temp[y0:y1, x0:x1, :] = 1.0
-    expOutM1.append(temp)
-    i += 1
-    if(i == size):
-        break
+    # Retrieve and process the images for the training and validation set
+    i = 0
+    for filename in os.listdir('flower/flower_photos'): 
+        # Radnomly generate a 4 letter word to be overlaid on an image
+        message = ''.join(choices(ascii_uppercase, k = 4))
+        # Randomly select a color for the font
+        c = 'rgb(' + str(randint(0, 255)) + ',' + str(randint(0, 255)) + ',' + str(randint(0, 255)) + ')'
+        # Draw the words on the images
+        images = drawText('flower/flower_photos/' + filename, fontsize, message, target_size, c)
+        textImages.append(np.array(images[0], dtype = float))
+        # Keep track of the location of the word for each image
+        temp = np.zeros(shape = (256, 256, 3), dtype = float)
+        x0 = images[1][0]
+        y0 = images[1][1]
+        x1 = images[1][2]
+        y1 = images[1][3]    
+        temp[y0:y1, x0:x1, :] = 1.0
+        expOutM1.append(temp)
+        i += 1
+        if(i == size):
+            break
 
-# Retriece and process the images for the testing set
-i = 0
-for filename in os.listdir('testingImages'):
-    # Radnomly generate a 4 letter word to be overlaid on an image
-    message = ''.join(choices(ascii_uppercase, k = 4))
-    images = drawText('testingImages/' + filename, fontsize, message, target_size, 'rgb(0,0,0)')
-    testTextImages.append(np.array(images[0], dtype = float))
-    i += 1
-    if(i == 15):
-        break
+    # Retriece and process the images for the testing set
+    i = 0
+    for filename in os.listdir('testingImages'):
+        # Radnomly generate a 4 letter word to be overlaid on an image
+        message = ''.join(choices(ascii_uppercase, k = 4))
+        images = drawText('testingImages/' + filename, fontsize, message, target_size, 'rgb(0,0,0)')
+        testTextImages.append(np.array(images[0], dtype = float))
+        i += 1
+        if(i == 15):
+            break
 
-# Convert datasets to NumPy arrays for use with TensorFlow
-textImages = np.array(textImages)
-expOutM1 = np.array(expOutM1)
-testTextImages = np.array(testTextImages)
+    # Convert datasets to NumPy arrays for use with TensorFlow
+    textImages = np.array(textImages)
+    expOutM1 = np.array(expOutM1)
+    testTextImages = np.array(testTextImages)
 
-# Normalize the rgb values in the datasets to fall between 0 and 1
-textImages[:,:,:,:] /= 255
-testTextImages[:,:,:,:] /= 255
+    # Normalize the rgb values in the datasets to fall between 0 and 1
+    textImages[:,:,:,:] /= 255
+    testTextImages[:,:,:,:] /= 255
 
-model_name = 'test_model.h5'
-epochs = 500
-#m = modelbuilder()
-#es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5)
-# Save the model everytime validation loss hits new minimum
-#mc = ModelCheckpoint(model_name, monitor = 'val_loss', mode = 'min', verbose = 1, save_best_only = True)
-# Train the model with the training dataset while reserving 20% of the data for validation
-#history = m.fit(x = textImages[:size][:][:][:], y = expOutM1[:size][:][:][:], batch_size = 100, epochs = epochs, validation_split = 0.2, use_multiprocessing = True, callbacks = mc)
+    if (loadModel == False):
+        epochs = 300
+        m = modelbuilder()
+        #Save the model everytime validation loss hits new minimum
+        mc = ModelCheckpoint(modelFilePath, monitor = 'val_loss', mode = 'min', verbose = 1, save_best_only = True)
+        #Train the model with the training dataset while reserving 20% of the data for validation
+        history = m.fit(x = textImages[:size][:][:][:], y = expOutM1[:size][:][:][:], batch_size = 100, epochs = epochs, validation_split = 0.2, use_multiprocessing = True, callbacks = mc)
 
-# Plot accuracy and loss results
-# plt.figure(figsize = (10, 10))
-# plt.subplot(1, 2, 1)
-# epochs_range = range(epochs)
-# plt.plot(epochs_range, history.history['accuracy'], label = 'Training Accuracy')
-# plt.plot(epochs_range, history.history['val_accuracy'], label = "validation Accuracy")
-# plt.legend(loc = 'lower right')
-# plt.title('Accuracy Results')
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, history.history['loss'], label = 'Training Loss')
-# plt.plot(epochs_range, history.history['val_loss'], label = "Validation Loss")
-# plt.legend(loc = 'upper right')
-# plt.title('Loss Results')
-# plt.savefig("results.png")
+        #Plot accuracy and loss results
+        plt.figure(figsize = (10, 10))
+        plt.subplot(1, 2, 1)
+        epochs_range = range(epochs)
+        plt.plot(epochs_range, history.history['accuracy'], label = 'Training Accuracy')
+        plt.plot(epochs_range, history.history['val_accuracy'], label = "validation Accuracy")
+        plt.legend(loc = 'lower right')
+        plt.title('Accuracy Results')
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, history.history['loss'], label = 'Training Loss')
+        plt.plot(epochs_range, history.history['val_loss'], label = "Validation Loss")
+        plt.legend(loc = 'upper right')
+        plt.title('Loss Results')
+        plt.savefig("results.png")
 
-#Predict on the testing dataaset using the saved model
-saved_model = load_model('test_model_S500E500.h5')
-results = saved_model.predict(testTextImages[:15][:][:][:])
-i = 0
+    #Predict on the testing dataaset using the saved model
+    saved_model = load_model(modelFilePath)
+    results = saved_model.predict(testTextImages[:15][:][:][:])
+    i = 0
 
-# Loop through the testing results and smooth the images based on the located text
-for images in results: 
-    gray = color.gray2rgb(images[:, :, 0])
-    screen = np.array(gray)
-    screen = screen / np.max(screen)
-    for r in range(256):
-        for c in range(256):
-            if screen[r,c,0] >= 0.92:
-                screen[r, c, 0] = 1
-                screen[r, c, 1] = 1
-                screen[r, c, 2] = 1
-            else:
-                screen[r, c, 0] = 0
-                screen[r, c, 1] = 0
-                screen[r, c, 2] = 0
-    # Save images with the text, detected text, and smoothed text
-    io.imsave('bwThreshold/' + str(i + 1) + ".png", (screen * 255).astype('uint8'), check_contrast=False)
-    io.imsave('testingImagesWithText/' + str(i + 1) + ".png", (testTextImages[i] * 255).astype('uint8'))
-    io.imsave('testingImagesDetectedText/' + str(i + 1) + ".png", (gray * 255).astype('uint8'), check_contrast=False)
-    smoothed = smooth(testTextImages[i], gray[:, :, 0] / np.max(gray))
-    io.imsave('testingImagesRemovedText/' + str(i + 1) + ".png", (smoothed * 255).astype('uint8'))
-    i +=1
+    # Loop through the testing results and smooth the images based on the located text
+    for images in results: 
+        gray = color.gray2rgb(images[:, :, 0])
+        screen = np.array(gray)
+        screen = screen / np.max(screen)
+        for r in range(256):
+            for c in range(256):
+                if screen[r,c,0] >= 0.92:
+                    screen[r, c, 0] = 1
+                    screen[r, c, 1] = 1
+                    screen[r, c, 2] = 1
+                else:
+                    screen[r, c, 0] = 0
+                    screen[r, c, 1] = 0
+                    screen[r, c, 2] = 0
+        # Save images with the text, detected text, and smoothed text
+        io.imsave('bwThreshold/' + str(i + 1) + ".png", (screen * 255).astype('uint8'), check_contrast=False)
+        io.imsave('testingImagesWithText/' + str(i + 1) + ".png", (testTextImages[i] * 255).astype('uint8'))
+        io.imsave('testingImagesDetectedText/' + str(i + 1) + ".png", (gray * 255).astype('uint8'), check_contrast=False)
+        smoothed = smooth(testTextImages[i], gray[:, :, 0] / np.max(gray))
+        io.imsave('testingImagesRemovedText/' + str(i + 1) + ".png", (smoothed * 255).astype('uint8'))
+        i +=1
+    return
